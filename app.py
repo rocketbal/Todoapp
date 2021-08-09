d3a07bfba27986d56fdc3b5c34831170b8bfb91f
Surinder Bal <dilraj.bal@hotmail.com>
2021-08-08 21:33:39 -0700

    Updates to close and completed funcs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,jsonify,abort
+from flask import Flask, render_template,request,jsonify,abort,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
@@ -10,15 +10,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
-
+# ---------------------------- Todo table declaration ------------------------------- #
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(),nullable=False)
+    completed = db.Column(db.Boolean,nullable=False,default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+# ---------------------------- Create a new item in the todo table ------------------------------- #
 @app.route('/todos/create',methods=["POST"])
 def create_todos():
     error = False
@@ -42,11 +44,27 @@ def create_todos():
     else:
         return jsonify(body)
 
-
-@app.route('/todos/<tocloseid>/close', methods=['POST'])
-def close_todo(tocloseid):
+# ----------------------------Update completed items with true ------------------------------- #
+@app.route('/todos/<todoId>/check-completed', methods=['POST'])
+def check_completed(todoId):
     try:
-        Todo.query.filter_by(id=tocloseid).delete()
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todoId)
+        todo.completed = completed
+        db.session.commit()
+
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+
+    return redirect(url_for('index'))
+
+# ----------------------------Close completed items ------------------------------- #
+@app.route('/todos/<tocloseId>/close', methods=['DELETE'])
+def close_todo(tocloseId):
+    try:
+        Todo.query.filter_by(id=tocloseId).delete()
         db.session.commit()
 
     except:
